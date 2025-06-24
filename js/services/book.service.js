927741547542b457fc68d4e0f0913b4c9419702c
Din Golan <dingolan100@gmail.com>
2025-06-23 19:24:28 -0500

    Description : - Add hardcoded book list with image URLs to book.service.js.

---
+++ js/services/book.service.js
@@ -0,0 +1,29 @@
+/*********************************/
+/* Exercise - Book Shop (Part 1) */
+/*********************************/
+'use strict';
+
+const gBooks = [
+    {
+        id: 'b101',
+        title: 'The Adventures of Lori Ipsi',
+        price: 120,
+        imgUrl: 'img/lori-ipsi.jpg'
+    },
+    {
+        id: 'b102',
+        title: 'World Atlas',
+        price: 300,
+        imgUrl: 'img/world-atlas.jpg'
+    },
+    {
+        id: 'b103',
+        title: 'Zorba the Greek',
+        price: 87,
+        imgUrl: 'img/zorba.jpg'
+    }
+]
+
+function getBooks() {
+    return gBooks;
+}
